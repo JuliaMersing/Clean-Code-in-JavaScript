3dfb2e6982f42ad9e07e890405946df9f3452aed
Maria Julia Mersing <mariajulia.mersing@juliamersing.home>
2021-12-14 15:22:46 +0100

    adding ESinheritance

---
+++ Lesson_1/ESinheritance/ESinheritance2.js
@@ -0,0 +1,23 @@
+class Animal {
+    constructor(legs) {
+        this.legs = legs;
+    }
+    walk() {
+        console.log('walking on ' + this.legs + ' legs');
+    }
+}
+
+class Bird extends Animal {
+    constructor(legs) {
+        super(legs);
+    }
+    fly() {
+        console.log('flying');
+    }
+}
+
+
+let bird = new Bird(2);
+
+bird.walk();
+bird.fly();
