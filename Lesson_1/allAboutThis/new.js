497da719237f89c5dc8ac21c49cc988434d5f441
Natalia Martin <natalia.martin@natalia.martin-laptop>
2021-12-15 15:58:58 +0100

    nothing new

--- Lesson_1/allAboutThis/new.js
+++ Lesson_1/allAboutThis/new.js
@@ -6,7 +6,7 @@ function Person(firstName, lastName, age) {
     console.log(this); //When using "new" to initialize a new object, "this" in the constructor function points to the newly constructed object. The result is the same that console.log(p).
 }
 
-let p = new Person("Ada", "Lovelace", 30);
+let p = new Person("Ada", "Lovelace", 30);// DOUBT: ¿por qué cuando se inicializa (¿o instancia?) un objeto se usa la keyword "let" y no "const"?
 console.log(p); 
 console.log(Person);
 
