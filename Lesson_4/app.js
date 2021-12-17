764e2d2ad36726dd5fbfd0237e815174b86abf54
Maria Julia Mersing <mariajulia.mersing@juliamersing.home>
2021-12-17 13:14:55 +0100

    error commit

--- Lesson_4/app.js
+++ Lesson_4/app.js
@@ -2,9 +2,8 @@ import {HttpClient} from './http-client';
 
 let http = new HttpClient();
 
-http.get("https://httpstat.us/404").then(data => {
+http.get("https://httpstat.us/401").then(data => {
     console.log(data);
 })
 
-//DOUBT: Si cambiamos el numero de error, no cambia el mensaje que vería el usuario
 //DOUBT: Aparece un undifined en la consola, que no sabemos de donde sale 
--- Lesson_4/webpack.config.js
+++ Lesson_4/webpack.config.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const path = require('path');
+
 module.exports = {
     entry : "./app.js", // This path is different from the video one
     mode: "development",
@@ -8,5 +10,14 @@ module.exports = {
     },
     resolve: {
         modules: ['node_modules'],// This path is different from the video one
-      }
+        extensions: ['', '.js', '.jsx', '.css']
+      },
+      devServer: {
+        hot: true, 
+        static: {
+          directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 8080,
+      },
 }
