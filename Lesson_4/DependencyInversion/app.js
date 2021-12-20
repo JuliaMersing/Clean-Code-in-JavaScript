a7e909b807d4354a8977464c8d1d130320646049
Julia Mersing <julia.mersing.ortiz@gmail.com>
2021-12-20 18:00:38 +0100

    add comments to DependencyInversion

--- Lesson_4/DependencyInversion/app.js
+++ Lesson_4/DependencyInversion/app.js
@@ -6,8 +6,8 @@ const bottle = new Bottle();
 bottle.service("HttpClient", HttpClient);
 bottle.service("PostsManager", PostsManager, "HttpClient"); //There is just one dependency
 
-let PostsManager = bottle.container.PostManager;
-postsManager.getPosts().then((posts) => console.log("Posts: ", posts));
+let PostsManager = bottle.container.PostManager; //This get the instance back
+postsManager.getPosts().then((posts) => console.log("Posts: ", posts)); //We get an instnce of PostManager without provide an instance of HttpClient at all, because the container knew how to instantiate PostManager without having us provided the necessary dependences.
 postsManager
   .getComments()
   .then((comments) => console.log("Comments: ", comments));
