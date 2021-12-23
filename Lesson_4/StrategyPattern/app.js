c4b6d438e81a1872b7e7367da566068a90e7c92b
Natalia Martin <natalia.martin@natalia.martin-laptop>
2021-12-23 10:28:18 +0100

    details fixed in StrategyPattern

--- Lesson_4/StrategyPattern/app.js
+++ Lesson_4/StrategyPattern/app.js
@@ -1,8 +1,9 @@
 import { ConsoleLogHandler } from "./console-log-handler";
-import { ErrorHandler, ErrorHandler } from "./error-handler";
+import { ErrorHandler } from "./error-handler";
 import { ToastrHandler } from "./toastr-handler";
 
-function configureErrorHandler() {
+//(This is a factory function)
+function configureErrorHandler() {//DOUBT: ¿Para qué hacer la factory si al final ella misma se va a guardar en una variable global?
   let errorHandler = new ErrorHandler(new ToastrHandler());
   return errorHandler;
 }
